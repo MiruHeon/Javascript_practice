cd0ae93a1bb68973558c12bb8018c5dc95373b9b
YOUSOL <imjamminii@gmail.com>
2025-09-14 00:40:10 +0900

    Day01.js

--- Day01.js
+++ Day01.js
@@ -1,51 +1,135 @@
-// Javascript는 백엔드를 구상하는 프로그래밍 언어중 하나로, 사이트의 동작을 담당한다.
+// JavaScript는 웹 브라우저에서 동작하는 프로그래밍 언어이자,
+// Node.js와 같은 환경에서는 서버(백엔드) 개발도 가능한 범용 언어입니다.
+// 주로 웹사이트의 동작과 상호작용을 담당합니다.
 
-// 출력하는 방법
-// console.log(출력값)
-//
-// 변수 선언 방법
-// var 변수 이름 = 할당값 (이름 중복 변수 선언 가능)
-// let 변수 이름 = 할당값 (이름 중복 변수 선언 불가)
-// const 변수 이름 = 할당값 (상수 즉, 값 변경이 불가)
-//
-// 변수 선언 시 주의할 점
-// 변수 이름에는 뛰어쓰기가 있어선 안됀다.
+// -----------------------------------
+// 1. 출력하는 방법
+// -----------------------------------
+// 화면에 값을 출력할 때는 console.log() 사용
+console.log("Hello, JavaScript!");
+console.log("숫자:", 123);
+console.log("합:", 10 + 20);
 
-var a = 10;
-let b = 20;
-const c = 30;
+// -----------------------------------
+// 2. 변수 선언
+// -----------------------------------
+// var : 기존 방식. 재선언 가능하지만, 스코프(scope)가 함수 단위여서 주의
+// let : ES6 이후 도입. 블록 단위 스코프, 재선언 불가
+// const : 상수. 선언 시 반드시 초기값 필요, 값 변경 불가
 
-console.log(a+b+c);
+var a = 10;   // 재선언 가능
+let b = 20;   // 재선언 불가
+const c = 30; // 상수, 값 변경 불가
 
-// 함수 선언 방법
-// function 함수이름(매개변수){함수 내용}
+console.log("a + b + c =", a + b + c);
 
-function func(x){
-  return x * 10;
+// -----------------------------------
+// 3. 자료형 (Type)
+// -----------------------------------
+// JavaScript는 동적 타입 언어
+let number = 100;      // 숫자(Number)
+let text = "Hello";    // 문자열(String)
+let isTrue = true;     // 불리언(Boolean)
+let nothing = null;    // null
+let notDefined;        // undefined
+
+console.log(number, text, isTrue, nothing, notDefined);
+
+// -----------------------------------
+// 4. 함수 선언
+// -----------------------------------
+// 1) 일반 함수
+function multiplyByTen(x) {
+    return x * 10;
 }
 
-// 조건문 선언 방법
-// if(조건식){참 일때 실행될 조건문 내용} , else if , else
+console.log("5 * 10 =", multiplyByTen(5));
 
-if(a > 5){
-  console.log("a가 5보다 크다.");
-}
-else{
-  console.log("a가 5이거나 5보다 작다.");
-}
+// 2) 화살표 함수 (ES6)
+const add = (x, y) => x + y;
+console.log("5 + 3 =", add(5, 3));
 
-// 반복문 선언 방법
-// for문
-// for(초기화식; 조건식; 증감식){반복 내용}
-//
-// while문
-// while(조건식){반복 내용}
-
-for(var i=0;i<10;i++){
-  console.log("i의 값 :",i);
+// -----------------------------------
+// 5. 조건문
+// -----------------------------------
+if (a > 5) {
+    console.log("a는 5보다 크다.");
+} else if (a === 5) {
+    console.log("a는 5이다.");
+} else {
+    console.log("a는 5보다 작다.");
 }
 
-while(a<100){
-  console.log("a의 값 :", a);
-  a++;
+// -----------------------------------
+// 6. 반복문
+// -----------------------------------
+
+// for문 : 반복 횟수가 명확할 때
+for (let i = 0; i < 5; i++) {
+    console.log(`for문 i의 값: ${i}`);
 }
+
+// while문 : 반복 조건이 참일 동안 반복
+while (a < 15) {
+    console.log(`while문 a의 값: ${a}`);
+    a++;
+}
+
+// do-while문 : 최소 한 번은 실행 후 조건 검사
+let j = 0;
+do {
+    console.log(`do-while j의 값: ${j}`);
+    j++;
+} while (j < 3);
+
+// -----------------------------------
+// 7. 배열
+// -----------------------------------
+let fruits = ["사과", "바나나", "체리"];
+console.log(fruits);
+console.log("첫 번째 과일:", fruits[0]);
+
+// 배열 반복
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`과일 ${i}:`, fruits[i]);
+}
+
+// 배열 메소드 예시
+fruits.push("오렌지"); // 뒤에 추가
+fruits.pop();           // 뒤에서 제거
+console.log("배열 after push/pop:", fruits);
+
+// -----------------------------------
+// 8. 객체 (Object)
+// -----------------------------------
+let person = {
+    name: "홍길동",
+    age: 20,
+    greet: function() {
+        console.log("안녕하세요, 저는 " + this.name + "입니다.");
+    }
+};
+
+console.log(person.name, person.age);
+person.greet();
+
+// -----------------------------------
+// 9. 이벤트/함수 활용 예시
+// -----------------------------------
+function sayHello(name) {
+    console.log(`${name}님, 안녕하세요!`);
+}
+
+sayHello("용헌");
+
+// -----------------------------------
+// 10. 요약
+// -----------------------------------
+// 1. 출력 : console.log()
+// 2. 변수 : var, let, const
+// 3. 자료형 : Number, String, Boolean, null, undefined
+// 4. 함수 : function, 화살표 함수
+// 5. 조건문 : if, else if, else
+// 6. 반복문 : for, while, do-while
+// 7. 자료구조 : 배열(Array), 객체(Object)
+// 8. 이벤트 및 함수 활용
